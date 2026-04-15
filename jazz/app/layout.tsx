5e7973c90e02da4c3ed8c60d917c7327bdb0b69e
Mileslegend <140729768+Mileslegend@users.noreply.github.com>
2026-04-15 09:33:57 +0000

    user name logic implemnted

---
+++ jazz/app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const jetbrainsMono = JetBrains_Mono({
+  variable: "--font-jetbrains-mono",
+  subsets: ["latin"],
+})
+
+
+export const metadata: Metadata = {
+  title: "Chitaz",
+  description: "They will never catch us, we are the jazz.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={` ${jetbrainsMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
