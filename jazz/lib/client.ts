06e8eb42a2f17d13d1f77f97d000de31b40d6945
Mileslegend <140729768+Mileslegend@users.noreply.github.com>
2026-04-17 15:39:14 +0000

    my instincts to solve CORS error

--- jazz/lib/client.ts
+++ jazz/lib/client.ts
@@ -2,6 +2,6 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // .api to enter /api prefix
-export const client = treaty<App>('https://urban-fiesta-pjrg95v7wj6g2j5w-3000.app.github.dev/').api
+export const client = treaty<App>('https://chita-two.vercel.app/').api
 const result = client.user.get()
   
